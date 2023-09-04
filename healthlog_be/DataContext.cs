5fb8f347a90f116dd00fc08b5f12157c96efeb09
TBout0 <tanner.boutelle@gmail.com>
2023-09-04 01:13:18 -0700

    added some stuff I copied to be from old proj be

---
+++ healthlog_be/DataContext.cs
@@ -0,0 +1,8 @@
+﻿namespace healthlog_be
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    }
+}
--- healthlog_be/Program.cs
+++ healthlog_be/Program.cs
@@ -1,3 +1,6 @@
+global using Microsoft.EntityFrameworkCore;
+using healthlog_be;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Allows the front end to access api
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "localhost",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+        });
+});
+// Connecting this .net to the mssql server which will run in a containerized docker
+builder.Services.AddDbContext<healthlog_be.DataContext>(options =>
+{
+    options.UseSqlServer("Server=localhost;Initial Catalog=PantryDB;Integrated Security=False;User Id=sa;Password=Your_password123;MultipleActiveResultSets=True")
+    .EnableSensitiveDataLogging();
+});
+/* Will need to add these services in order to access each service api
+builder.Services.AddScoped<IFamilyService, FamilyService>();
+builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IPantryService, PantryService>();
+builder.Services.AddScoped<IRecipeService, RecipeService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IInviteService,InviteService>();
+*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +40,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+// Automatically creates Migration maybe
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
